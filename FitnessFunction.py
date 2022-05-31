e3a7853c8a0ed98174b33c93f9e0af5dc730eeb9
MiguelFU <106198273+MiguelFU@users.noreply.github.com>
2022-05-30 19:27:53 -0500

    Update FitnessFunction.py

--- FitnessFunction.py
+++ FitnessFunction.py
@@ -10,10 +10,17 @@ Author: Miguel Angel Flores Urbina
         Luis Angel Dias Navas
         Leonardo Villanueva Medina
 
+Profesor: Dr. Asdrúbal López Chau
+
 Descripción:  Clase fitnessfunction
 evalua individuos para el problema de polinomios
 
 Created on Mon May  22 11:13:53 2022
+
+Enlace del video: https://www.youtube.com/watch?v=C9F6cKH1KGQ
+
+Enlace de GitGub: https://github.com/MiguelFU/Algoritmos-Geneticos-Proyecto-2?fbclid=IwAR0aGxPM3mDM3gG9TPONVleBuLGNvjqgHC7O-kxEWMUQ0bRCsZLIOxbN6g8
+
 
 @author: angel
 """
@@ -21,7 +28,7 @@ import numpy as np
 class FitnessFunction:
 
     def __init__(self, target):
-        #Es la contraseña, en caso de que no se coloque nada, la contraseña por defecto sera "holam"
+        #Es la evaluacion 
         self.target = target
         self.lamda = 1
         self.beta = 1
@@ -43,9 +50,9 @@ class FitnessFunction:
             letras en la posicion correcta
             '''
         #print("ESTE ES TARGET ", self.target)
-        ladosCaja = ind.cromosoma.getValues()
+        ladC = ind.cromosoma.getValues()
         vol = 1
-        for lado in ladosCaja:
+        for lado in ladC:
             vol = vol * lado
         #print(self.target - vol)
         x = np.abs(self.target - vol)
